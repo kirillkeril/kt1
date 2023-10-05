0082d9ccabd91ea46c6634d930fbe7d105e4e518
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-10-05 11:35:06 +0500

    first commit

---
+++ bin/kt1.dart
@@ -0,0 +1,5 @@
+import 'package:kt1/kt1.dart' as kt1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kt1.calculate()}!');
+}
---
+++ test/kt1_test.dart
@@ -0,0 +1,8 @@
+import 'package:kt1/kt1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
